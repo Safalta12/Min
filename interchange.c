7932c2b4dd714144ad9d36915dbc2112fcae0d41
Safalta Shrestha <safalta1@gmail.com>
2024-03-11 19:01:52 +0545

    add interchange

---
+++ interchange.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,temp;
+    printf("n1");
+    scanf("%d",n1);
+    printf("n2");
+    scanf(" %d ",n2);
+    temp=n1;
+    n1=n2;
+    n2=temp;
+    printf("The number after interchanging the contents are n1=%d and n2=%d",n1,n2);
+    return 0;
+}
