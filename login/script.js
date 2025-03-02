2208cdb04d421f7fc2d39873f3eadd9c2ca392a6
Nanii <141534902+Safalta12@users.noreply.github.com>
2025-03-02 18:08:02 +0545

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,22 +1,16 @@
-const pwShowHide = document.querySelectorAll(".icon-a");
+document.addEventListener("DOMContentLoaded", () => {
+    const pwShowHide = document.querySelectorAll(".icon-a");
 
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwField = eyeIcon.previousElementSibling;
-        if (pwField.type === "password") {
-            pwField.type = "text";
-            eyeIcon.classList.replace("bx-hide", "bx-show");
-        } else {
-            pwField.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
-        }
+    pwShowHide.forEach(eyeIcon => {
+        eyeIcon.addEventListener("click", () => {
+            let pwField = eyeIcon.previousElementSibling;
+            if (pwField.type === "password") {
+                pwField.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+            } else {
+                pwField.type = "password";
+                eyeIcon.classList.replace("bx-show", "bx-hide");
+            }
+        });
     });
 });
-
-function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 909a7c65154efa52fd43b84ae68d1330a2910720
